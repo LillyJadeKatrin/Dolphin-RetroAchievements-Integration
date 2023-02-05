2864b6ab466a5107d2e0b312134e1ed0300ff8f4
LillyJadeKatrin <lilly.kitty.1988@gmail.com>
2023-02-05 08:37:42 -0500

    RetroAchievements Integration version 0.5.1 * Added a welcome dialog when the player is already logged in to retroachievements.org upon startup, maybe ought to go in its own class but w/e

--- Source/Core/DolphinQt/MainWindow.h
+++ Source/Core/DolphinQt/MainWindow.h
@@ -165,6 +165,7 @@ private:
   void ShowCheatsManager();
   void ShowRiivolutionBootWidget(const UICommon::GameFile& game);
   void ShowAchievementsWindow();
+  void ShowAchievementsWelcome();
   void ActivateRAMenuItem(int id);
 
   void NetPlayInit();
